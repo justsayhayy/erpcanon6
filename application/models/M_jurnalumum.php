e6f351491964d8313cd7c7010209ae29d847d27e
Achmad Arif Munaji <arief.munaji65@gmail.com>
2022-06-18 03:44:49 +0700

    Perbaikan halaman jurnalumum beserta fitur filter/search

--- application/models/M_jurnalumum.php
+++ application/models/M_jurnalumum.php
@@ -1,4 +1,4 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 class M_jurnalumum extends CI_Model
 {
@@ -12,7 +12,8 @@ class M_jurnalumum extends CI_Model
     //     return $query->result_array();
     // }
 
-    public function tampil_data($weekending) {
+    public function tampil_data($weekending)
+    {
         $this->db->order_by('id', 'DESC');
         if ($weekending != "") {
             if ($weekending == "up") {
@@ -24,46 +25,73 @@ class M_jurnalumum extends CI_Model
         }
     }
 
-    public function getWeekending() {
+    public function selectingData($select)
+    {
+        if ($select == 'all') {
+            return $this->db->get('jurnalumum')->result_array();
+        } else if ($select == 'up') {
+            return $this->db->get_where('jurnalumum', ['tutup_buku' => "T"])->result_array();
+        } else {
+            return $this->db->get_where('jurnalumum', array('tgl' => $select, 'tutup_buku' => 'Y'))->result_array();
+        }
+    }
+
+    public function getWeekending()
+    {
         $this->db->select('tgl');
+        $this->db->distinct();
         $this->db->where('tutup_buku', 'Y');
         return $this->db->get('jurnalumum')->result_array();
     }
 
-    public function getAllAccount() {
+    public function getTransaksi($teks)
+    {
+        $this->db->select('*');
+        $this->db->like('transaksi', $teks);
+        return $this->db->get('jurnalumum')->result_array();
+    }
+
+
+    public function getAllAccount()
+    {
         $this->db->select('kode, nama');
         $this->db->where('akun_user', 'T');
         return $this->db->get('chartofaccount')->result_array();
     }
 
-    public function getAccountName($kode) {
+    public function getAccountName($kode)
+    {
         $this->db->select('nama');
         $this->db->where('kode', $kode);
         return $this->db->get('chartofaccount')->row_array();
     }
 
-    public function getLatestJPId() {
+    public function getLatestJPId()
+    {
         $this->db->select('no_bukti');
         $this->db->order_by('id', 'DESC');
         $this->db->limit(1);
-        $this->db->like('no_bukti','JP');
+        $this->db->like('no_bukti', 'JP');
         return $this->db->get('jurnalumum')->row_array();
     }
 
-    public function getLatestJRId() {
+    public function getLatestJRId()
+    {
         $this->db->select('no_bukti');
         $this->db->order_by('id', 'DESC');
         $this->db->limit(1);
-        $this->db->like('no_bukti','JR');
+        $this->db->like('no_bukti', 'JR');
         return $this->db->get('jurnalumum')->row_array();
     }
-    
-    public function getDataById($id) {
+
+    public function getDataById($id)
+    {
         $this->db->where('id', $id);
         return $this->db->get('jurnalumum')->row_array();
     }
 
-    public function getDataByWord($word) {
+    public function getDataByWord($word)
+    {
         if ($word && $word != "-") {
             $this->db->like('transaksi', $word);
         }
@@ -71,32 +99,33 @@ class M_jurnalumum extends CI_Model
         return $this->db->get('jurnalumum')->result_array();
     }
 
-    public function tambahDataJurnal() {
-        $debit = $this->cekSaldoAkun($this->input->post('kode_debit',true));
-        $kredit = $this->cekSaldoAkun($this->input->post('kode_kredit',true));
+    public function tambahDataJurnal()
+    {
+        $debit = $this->cekSaldoAkun($this->input->post('kode_debit', true));
+        $kredit = $this->cekSaldoAkun($this->input->post('kode_kredit', true));
 
         if (!$debit) {
             $this->db->select('saldo_default');
             $this->db->from('chartofaccount');
-            $this->db->where('kode', $this->input->post('kode_debit',true));
+            $this->db->where('kode', $this->input->post('kode_debit', true));
             $s = $this->db->get()->row_array();
-            $saldo_debit = (int)$s['saldo_default'] + (int)$this->input->post('didebit',true);
-            $this->tambahLogAkun(0, $saldo_debit, (int)$this->input->post('kode_debit',true));
+            $saldo_debit = (int)$s['saldo_default'] + (int)$this->input->post('didebit', true);
+            $this->tambahLogAkun(0, $saldo_debit, (int)$this->input->post('kode_debit', true));
         } else {
-            $saldo_debit = (int)$debit['saldo_akhir'] + (int)$this->input->post('didebit',true);
-            $this->tambahLogAkun($debit['saldo_akhir'], $saldo_debit, (int)$this->input->post('kode_debit',true));
+            $saldo_debit = (int)$debit['saldo_akhir'] + (int)$this->input->post('didebit', true);
+            $this->tambahLogAkun($debit['saldo_akhir'], $saldo_debit, (int)$this->input->post('kode_debit', true));
         }
 
         if (!$kredit) {
             $this->db->select('saldo_default');
             $this->db->from('chartofaccount');
-            $this->db->where('kode', $this->input->post('kode_kredit',true));
+            $this->db->where('kode', $this->input->post('kode_kredit', true));
             $s = $this->db->get()->row_array();
-            $saldo_kredit = (int)$s['saldo_default'] - (int)$this->input->post('dikredit',true);
-            $this->tambahLogAkun(0, $saldo_kredit, (int)$this->input->post('kode_kredit',true));
+            $saldo_kredit = (int)$s['saldo_default'] - (int)$this->input->post('dikredit', true);
+            $this->tambahLogAkun(0, $saldo_kredit, (int)$this->input->post('kode_kredit', true));
         } else {
-            $saldo_kredit = (int)$kredit['saldo_akhir'] - (int)$this->input->post('dikredit',true);
-            $this->tambahLogAkun($kredit['saldo_akhir'], $saldo_kredit, (int)$this->input->post('kode_kredit',true));
+            $saldo_kredit = (int)$kredit['saldo_akhir'] - (int)$this->input->post('dikredit', true);
+            $this->tambahLogAkun($kredit['saldo_akhir'], $saldo_kredit, (int)$this->input->post('kode_kredit', true));
         }
 
         $date = date_create($this->input->post('tgl', true));
@@ -104,39 +133,39 @@ class M_jurnalumum extends CI_Model
         $data = [
             // "product_id"=>$this->input->post('product_id',true),
             "tgl" => $this->input->post('tgl', true),
-            "transaksi" => $this->input->post('transaksi',true),
-            "no_bukti" => $this->input->post('no_bukti',true),
-            "jumlah" => $this->input->post('jumlah',true),
-            "kode_debit" => $this->input->post('kode_debit',true),
-            "kode_kredit" => $this->input->post('kode_kredit',true),
-            "nama_akundebit" => $this->input->post('nama_akundebit',true),
-            "didebit" => $this->input->post('didebit',true),
-            "nama_akunkredit" => $this->input->post('nama_akunkredit',true),
-            "dikredit" => $this->input->post('dikredit',true),
+            "transaksi" => $this->input->post('transaksi', true),
+            "no_bukti" => $this->input->post('no_bukti', true),
+            "jumlah" => $this->input->post('jumlah', true),
+            "kode_debit" => $this->input->post('kode_debit', true),
+            "kode_kredit" => $this->input->post('kode_kredit', true),
+            "nama_akundebit" => $this->input->post('nama_akundebit', true),
+            "didebit" => $this->input->post('didebit', true),
+            "nama_akunkredit" => $this->input->post('nama_akunkredit', true),
+            "dikredit" => $this->input->post('dikredit', true),
             "tutup_buku" => "T"
         ];
 
         $data2 = [[
             // "product_id"=>$this->input->post('product_id',true),
             "tgl" => date_format($date, 'Y-m-d'),
-            "kode" => $this->input->post('kode_debit',true),
-            "transaksi" => $this->input->post('transaksi',true),
-            "no_bukti" => $this->input->post('no_bukti',true),
-            "debit" => $this->input->post('didebit',true),
+            "kode" => $this->input->post('kode_debit', true),
+            "transaksi" => $this->input->post('transaksi', true),
+            "no_bukti" => $this->input->post('no_bukti', true),
+            "debit" => $this->input->post('didebit', true),
             "kredit" => null,
             "tutup_buku" => "T"
-          
+
             // "image"=>$this->image = $this->_uploadImage()
-        ],[
+        ], [
             // "product_id"=>$this->input->post('product_id',true),
             "tgl" => date_format($date, 'Y-m-d'),
-            "kode" => $this->input->post('kode_kredit',true),
-            "transaksi" => $this->input->post('transaksi',true),
-            "no_bukti" => $this->input->post('no_bukti',true),
+            "kode" => $this->input->post('kode_kredit', true),
+            "transaksi" => $this->input->post('transaksi', true),
+            "no_bukti" => $this->input->post('no_bukti', true),
             "debit" => null,
-            "kredit" => $this->input->post('dikredit',true),
+            "kredit" => $this->input->post('dikredit', true),
             "tutup_buku" => "T"
-          
+
             // "image"=>$this->image = $this->_uploadImage()
         ]];
 
@@ -144,7 +173,8 @@ class M_jurnalumum extends CI_Model
         $this->db->insert_batch('bukubesar', $data2);
     }
 
-    public function cekSaldoAkun($kode) {
+    public function cekSaldoAkun($kode)
+    {
         $this->db->select('saldo_awal, saldo_akhir, selisih');
         $this->db->from('account_log');
         $this->db->where('kode', $kode);
@@ -152,7 +182,8 @@ class M_jurnalumum extends CI_Model
         return $this->db->get()->row_array();
     }
 
-    public function tambahLogAkun($saldo_awal, $saldo_debkre, $kode) {
+    public function tambahLogAkun($saldo_awal, $saldo_debkre, $kode)
+    {
 
         $data_acclog = [
             'saldo_awal' => $saldo_awal,
@@ -165,39 +196,41 @@ class M_jurnalumum extends CI_Model
         $this->db->update('account_log', $data_acclog);
         return;
     }
-    
-    public function getJurnalById($id){
-        return $this->db->get_where('jurnalumum',['id'=>$id])->row_array();
+
+    public function getJurnalById($id)
+    {
+        return $this->db->get_where('jurnalumum', ['id' => $id])->row_array();
     }
-    public function ubahDataJurnal() {
-        $debit = $this->cekSaldoAkun($this->input->post('kode_debit2',true));
-        $kredit = $this->cekSaldoAkun($this->input->post('kode_kredit2',true));
+    public function ubahDataJurnal()
+    {
+        $debit = $this->cekSaldoAkun($this->input->post('kode_debit2', true));
+        $kredit = $this->cekSaldoAkun($this->input->post('kode_kredit2', true));
 
         if (!$debit) {
             $this->db->select('saldo_default');
             $this->db->from('chartofaccount');
-            $this->db->where('kode', $this->input->post('kode_debit2',true));
+            $this->db->where('kode', $this->input->post('kode_debit2', true));
             $s = $this->db->get()->row_array();
-            $saldo_debit = (int)$s['saldo_default'] + (int)$this->input->post('didebit2',true);
-            $this->tambahLogAkun(0, $saldo_debit, (int)$this->input->post('kode_debit2',true));
+            $saldo_debit = (int)$s['saldo_default'] + (int)$this->input->post('didebit2', true);
+            $this->tambahLogAkun(0, $saldo_debit, (int)$this->input->post('kode_debit2', true));
         } else {
-            $saldo_debit = (int)$debit['saldo_awal'] + (int)$this->input->post('didebit2',true);
-            $this->tambahLogAkun($debit['saldo_awal'], $saldo_debit, (int)$this->input->post('kode_debit2',true));
+            $saldo_debit = (int)$debit['saldo_awal'] + (int)$this->input->post('didebit2', true);
+            $this->tambahLogAkun($debit['saldo_awal'], $saldo_debit, (int)$this->input->post('kode_debit2', true));
         }
 
         if (!$kredit) {
             $this->db->select('saldo_default');
             $this->db->from('chartofaccount');
-            $this->db->where('kode', $this->input->post('kode_kredit2',true));
+            $this->db->where('kode', $this->input->post('kode_kredit2', true));
             $s = $this->db->get()->row_array();
-            $saldo_kredit = (int)$s['saldo_default'] - (int)$this->input->post('dikredit2',true);
-            $this->tambahLogAkun(0, $saldo_kredit, (int)$this->input->post('kode_kredit2',true));
+            $saldo_kredit = (int)$s['saldo_default'] - (int)$this->input->post('dikredit2', true);
+            $this->tambahLogAkun(0, $saldo_kredit, (int)$this->input->post('kode_kredit2', true));
         } else {
-            $saldo_kredit = (int)$kredit['saldo_awal'] - (int)$this->input->post('dikredit2',true);
-            $this->tambahLogAkun($kredit['saldo_awal'], $saldo_kredit, (int)$this->input->post('kode_kredit2',true));
+            $saldo_kredit = (int)$kredit['saldo_awal'] - (int)$this->input->post('dikredit2', true);
+            $this->tambahLogAkun($kredit['saldo_awal'], $saldo_kredit, (int)$this->input->post('kode_kredit2', true));
         }
 
-        $this->db->where('no_bukti', $this->input->post('no_bukti2',true));
+        $this->db->where('no_bukti', $this->input->post('no_bukti2', true));
         $this->db->delete('bukubesar');
 
         $date = date_create($this->input->post('tgl2', true));
@@ -205,39 +238,39 @@ class M_jurnalumum extends CI_Model
         $data2 = [[
             // "product_id"=>$this->input->post('product_id',true),
             "tgl" => date_format($date, 'Y-m-d'),
-            "kode" => $this->input->post('kode_debit2',true),
-            "transaksi" => $this->input->post('transaksi2',true),
-            "no_bukti" => $this->input->post('no_bukti2',true),
-            "debit" => $this->input->post('didebit2',true),
+            "kode" => $this->input->post('kode_debit2', true),
+            "transaksi" => $this->input->post('transaksi2', true),
+            "no_bukti" => $this->input->post('no_bukti2', true),
+            "debit" => $this->input->post('didebit2', true),
             "kredit" => null,
             "tutup_buku" => "T"
-          
+
             // "image"=>$this->image = $this->_uploadImage()
-        ],[
+        ], [
             // "product_id"=>$this->input->post('product_id',true),
             "tgl" => date_format($date, 'Y-m-d'),
-            "kode" => $this->input->post('kode_kredit2',true),
-            "transaksi" => $this->input->post('transaksi2',true),
-            "no_bukti" => $this->input->post('no_bukti2',true),
+            "kode" => $this->input->post('kode_kredit2', true),
+            "transaksi" => $this->input->post('transaksi2', true),
+            "no_bukti" => $this->input->post('no_bukti2', true),
             "debit" => null,
-            "kredit" => $this->input->post('dikredit2',true),
+            "kredit" => $this->input->post('dikredit2', true),
             "tutup_buku" => "T"
-          
+
             // "image"=>$this->image = $this->_uploadImage()
         ]];
-        
+
         $this->db->insert_batch('bukubesar', $data2);
-        
+
         $data = [
-            "transaksi"=>$this->input->post('transaksi2',true),
-            "no_bukti"=>$this->input->post('no_bukti2',true),
-            "jumlah"=>$this->input->post('jumlah2',true),
-            "kode_debit"=>$this->input->post('kode_debit2',true),
-            "kode_kredit"=>$this->input->post('kode_kredit2',true),
-            "nama_akundebit"=>$this->input->post('nama_akundebit2',true),
-            "didebit"=>$this->input->post('didebit2',true),
-            "nama_akunkredit"=>$this->input->post('nama_akunkredit2',true),
-            "dikredit"=>$this->input->post('dikredit2',true)
+            "transaksi" => $this->input->post('transaksi2', true),
+            "no_bukti" => $this->input->post('no_bukti2', true),
+            "jumlah" => $this->input->post('jumlah2', true),
+            "kode_debit" => $this->input->post('kode_debit2', true),
+            "kode_kredit" => $this->input->post('kode_kredit2', true),
+            "nama_akundebit" => $this->input->post('nama_akundebit2', true),
+            "didebit" => $this->input->post('didebit2', true),
+            "nama_akunkredit" => $this->input->post('nama_akunkredit2', true),
+            "dikredit" => $this->input->post('dikredit2', true)
         ];
 
         // if (!empty($_FILES["image"]["name"])) {
@@ -245,8 +278,8 @@ class M_jurnalumum extends CI_Model
         // }else{
         //     $this->image = $post["old_image"];
         // }
-        $this->db->where('id',$this->input->post('id'));
-        $this->db->update('jurnalumum',$data);
+        $this->db->where('id', $this->input->post('id'));
+        $this->db->update('jurnalumum', $data);
         // $post = $this->input->post();
         // $this->product_id = $post["product_id"];
         // $this->name = $post["name"];
@@ -255,18 +288,20 @@ class M_jurnalumum extends CI_Model
         // $this->hargajual = $post["hargajual"];
         // $this->hargabeli = $post["hargabeli"];
         // $this->detail = $post["detail"];
-        
-        
+
+
         // $this->db->update($this->_table,$this,array('id' =>$post['id']));
     }
 
-    public function deleteBukubesarLog($kode_akun, $no_bukti) {
+    public function deleteBukubesarLog($kode_akun, $no_bukti)
+    {
         $this->db->where('kode', $kode_akun);
         $this->db->where('no_bukti', $no_bukti);
         $this->db->delete('bukubesar');
     }
 
-    public function deleteAccountLog($kode_akun, $debkre, $jenis) {
+    public function deleteAccountLog($kode_akun, $debkre, $jenis)
+    {
         $this->db->select('saldo_awal, saldo_akhir, selisih');
         $this->db->where('kode', $kode_akun);
         $this->db->where('tanggal', 'up');
@@ -288,7 +323,8 @@ class M_jurnalumum extends CI_Model
         $this->db->update('account_log', $data);
     }
 
-    public function hapusDataJurnal($id) {
+    public function hapusDataJurnal($id)
+    {
         $this->db->select('kode_debit, kode_kredit, no_bukti, didebit, dikredit');
         $this->db->where('id', $id);
         $kode = $this->db->get('jurnalumum')->row_array();
